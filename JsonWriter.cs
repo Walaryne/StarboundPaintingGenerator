d7d987feebbf275102be9b7ac8439f6eb6cb64b5
Walaryne <hazardoustheradioactive@gmail.com>
2021-12-18 20:44:08 -0500

    Rolled ahead to NET6, converted JsonWriter to use JsonNode

--- JsonWriter.cs
+++ JsonWriter.cs
@@ -1,8 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 namespace StarboundPaintingGenerator
 {
     public class JsonWriter
@@ -32,9 +33,9 @@ namespace StarboundPaintingGenerator
         private static void WriteFrames(Item item)
         {
             using StreamWriter sw = File.CreateText($"{item.dir}/{item.itemname}.frames");
-            JObject frames = JObject.Parse(Util.GetEmbeddedResourceContent("StarboundPaintingGenerator.resources.example.frames"));
-            var frameGrid = (JObject)frames["frameGrid"];
-            var size = (JArray)frameGrid?["size"];
+            JsonNode frames = JsonNode.Parse(Util.GetEmbeddedResourceContent("StarboundPaintingGenerator.resources.example.frames"));
+            JsonNode frameGrid = frames?["frameGrid"];
+            JsonNode size = frameGrid?["size"];
 
             if (size != null)
             {
@@ -42,79 +43,107 @@ namespace StarboundPaintingGenerator
                 size[1] = item.height;
             }
 
-            using var writer = new JsonTextWriter(sw) {Formatting = Formatting.Indented};
-            frames.WriteTo(writer);
+            var writerOptions = new JsonWriterOptions
+            {
+                Indented = true
+            };
+            
+            using var writer = new Utf8JsonWriter(sw.BaseStream, writerOptions);
+            frames?.WriteTo(writer);
         }
         
         private static void WriteObject(Item item)
         {
             using StreamWriter sw = File.CreateText($"{item.dir}/{item.itemname}.object");
-            JObject obj = JObject.Parse(Util.GetEmbeddedResourceContent("StarboundPaintingGenerator.resources.example.object"));
+            JsonNode obj = JsonNode.Parse(Util.GetEmbeddedResourceContent("StarboundPaintingGenerator.resources.example.object"));
             string title = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(item.itemname);
-            
+
+            if (obj == null)
+                return;
             obj["objectName"] = item.itemname;
             obj["description"] = $"A painting by {title}";
             obj["shortdescription"] = title;
             obj["inventoryIcon"] = $"{item.itemname}.png";
 
-            var orientations = (JArray)obj["orientations"];
-            
-            if (orientations != null)
+            JsonNode orientations = obj["orientations"];
+            JsonNode orientation = orientations?[0];
+            if (orientation != null)
             {
-                var orientation = (JObject)orientations[0];
                 orientation["image"] = $"{item.itemname}.png:<color>";
             }
-            
-            using var writer = new JsonTextWriter(sw) {Formatting = Formatting.Indented};
+
+            var writerOptions = new JsonWriterOptions
+            {
+                Indented = true
+            };
+
+            using var writer = new Utf8JsonWriter(sw.BaseStream, writerOptions);
             obj.WriteTo(writer);
         }
         
         private static void WriteRecipe(Item item)
         {
             using StreamWriter sw = File.CreateText($"{item.dir}/{item.itemname}.recipe");
-            JObject recipe = JObject.Parse(Util.GetEmbeddedResourceContent("StarboundPaintingGenerator.resources.example.recipe"));
+            JsonNode recipe = JsonNode.Parse(Util.GetEmbeddedResourceContent("StarboundPaintingGenerator.resources.example.recipe"));
 
-            var output = (JObject)recipe["output"];
-            if (output != null)
+            JsonNode output = recipe?["output"];
+            if (output is not null)
             {
                 output["item"] = item.itemname;
             }
 
-            using var writer = new JsonTextWriter(sw) {Formatting = Formatting.Indented};
-            recipe.WriteTo(writer);
+            var writerOptions = new JsonWriterOptions
+            {
+                Indented = true
+            };
+            
+            using var writer = new Utf8JsonWriter(sw.BaseStream, writerOptions);
+            recipe?.WriteTo(writer);
         }
         
         private void WritePatches(string dir)
         {
             using StreamWriter sw = File.CreateText($"{dir}/player.config.patch");
-            JArray patch = JArray.Parse(Util.GetEmbeddedResourceContent("StarboundPaintingGenerator.resources.player.config.patch"));
-            var spArray = (JArray)patch[0];
-            var initialstate = (JArray)spArray.DeepClone();
+            JsonNode patch = JsonNode.Parse(Util.GetEmbeddedResourceContent("StarboundPaintingGenerator.resources.player.config.patch"));
+            JsonNode patchCopy = JsonNode.Parse(Util.GetEmbeddedResourceContent("StarboundPaintingGenerator.resources.player.config.patch"));
+            
+            JsonArray spArray = patch?.AsArray();
+            JsonArray initialCopy = patchCopy?.AsArray();
 
-            spArray.Remove();
+            spArray?.RemoveAt(0);
 
             foreach (Item item in items)
             {
-                var entry = (JObject)initialstate[0];
-                var value = (JObject)entry["value"];
-                if (value != null)
+                JsonArray entry = initialCopy?[0]?.AsArray();
+                JsonObject value = entry?[0]?.AsObject();
+                if (value is not null)
                 {
                     value["item"] = item.itemname;
                 }
-                patch.Add(initialstate.DeepClone());
+                spArray?.Add(JsonNode.Parse(entry?.ToJsonString() ?? throw new InvalidOperationException())?.AsArray());
             }
             
-            using var writer = new JsonTextWriter(sw) {Formatting = Formatting.Indented};
-            patch.WriteTo(writer);
+            var writerOptions = new JsonWriterOptions
+            {
+                Indented = true
+            };
+            
+            using var writer = new Utf8JsonWriter(sw.BaseStream, writerOptions);
+            patch?.WriteTo(writer);
         }
         
         private static void WriteMetadata(string dir)
         {
             using StreamWriter sw = File.CreateText($"{dir}/_metadata");
-            JObject metadata = JObject.Parse(Util.GetEmbeddedResourceContent("StarboundPaintingGenerator.resources._metadata"));
+            JsonNode metadata = JsonNode.Parse(Util.GetEmbeddedResourceContent("StarboundPaintingGenerator.resources._metadata"));
             
-            using var writer = new JsonTextWriter(sw) {Formatting = Formatting.Indented};
-            metadata.WriteTo(writer);
+            var writerOptions = new JsonWriterOptions
+            {
+                Indented = true
+            };
+            
+            using var writer = new Utf8JsonWriter(sw.BaseStream, writerOptions);
+            metadata?.WriteTo(writer);
         }
     }
 }
